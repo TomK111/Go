7842155a6503e38a106e2a04bd086cf2c424586a
Tomislav Kraljic <tomislavkraljic@outlook.com>
2021-01-24 03:24:48 -0500

    started notes on nil value

---
+++ control_flow_and_error_handling/error_handling/nil/main.go
@@ -0,0 +1,20 @@
+package main
+
+// nil value means the value is not initialized yet
+// nil == null(javascript)
+// nil == null(c# or java)
+// nil gives zero value for pointer-based types such as : pointers, slices, maps, interfaces and channels.
+
+/*
+err := do()
+if err != nil {
+	error
+}
+
+//success...continue
+
+*/
+
+func main() {
+
+}
